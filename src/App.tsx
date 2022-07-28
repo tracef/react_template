5b48a1d45cd682cda0176a83c04470ce14c8383a
Trace Flynn <tracemfl@gmail.com>
2022-07-28 01:20:48 -0400

    init

---
+++ src/App.tsx
@@ -0,0 +1,8 @@
+const App = () => {
+    return (
+        <div className={'main'}>
+            <h1>This is a React template using Typescript and SCSS</h1>
+        </div>
+    )
+}
+export default App;
